70801ab878d467eeaa6a58f48422a499cd015812
Nikki A <nikki.apostolakis@gmail.com>
2019-02-18 20:50:11 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
 
     @user = User.find_by(name: params[:user][:name])
 
-    if @user.authenticate(params[:password])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
 
     redirect_to controller: 'users',  action: 'welcome'
   end
