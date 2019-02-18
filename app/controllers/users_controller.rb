204e2d1738795eef01f063e2592dcde9be6bb368
Nikki A <nikki.apostolakis@gmail.com>
2019-02-18 21:08:27 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
 
   def create
     #sign up
+    @user = User.create(user_params)
+
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to controller: 'users',  action: 'welcome'
+    else
+      redirect_to 'new'
+    end
+    
 
   end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+  
 end
