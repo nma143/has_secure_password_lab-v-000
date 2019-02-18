c39f906229f74f5f5b8fa4c9bdee95d1164fb042
Nikki A <nikki.apostolakis@gmail.com>
2019-02-18 20:49:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #user is trying to log in
+    
+    @user = User.find_by(name: params[:user][:name])
 
+    if @user.authenticate(params[:password])
+    
     redirect_to controller: 'users',  action: 'welcome'
   end
 end
